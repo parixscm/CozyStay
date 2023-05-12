c4c8fe80a292ecd01936fc11a1eab9ef6cd9f8c9
parixscm <parixscm@gmail.com>
2023-05-12 13:59:39 +0900

    🎉 프로젝트 세팅

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import "./globals.css";
+import { Gowun_Dodum } from "next/font/google";
+
+const gowun_dodum = Gowun_Dodum({ subsets: ["latin"], weight: "400" });
+
+export const metadata = {
+  title: "My CozyStay",
+  description: "Stay while away",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={gowun_dodum.className}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>프로젝트 시작합니다!</h1>
+    </div>
+  );
+}
